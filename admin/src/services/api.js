5543fa7114541f16c8dbd253fcfdc6058f80a31b
LyneAssaad <assaadlyne@gmail.com>
2026-07-23 17:10:11 +0300

    Fix production API URL for Vercel frontend

--- admin/src/services/api.js
+++ admin/src/services/api.js
@@ -1,7 +1,28 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://taskflow-internship-production.up.railway.app",
 });
+
+
+api.interceptors.request.use(
+  (config) => {
+
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = token;
+    }
+
+    return config;
+
+  },
+  (error) => {
+
+    return Promise.reject(error);
+
+  }
+);
+
 
 export default api;
